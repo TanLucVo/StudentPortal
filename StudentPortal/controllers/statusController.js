91a5bb43b545bc31233ecf3e1f4a163f8ae12d56
HuynhLoi <caoboiloi@users.noreply.github.com>
2021-03-17 07:54:13 +0700

    api status: getAllStatus

--- StudentPortal/controllers/statusController.js
+++ StudentPortal/controllers/statusController.js
@@ -6,7 +6,7 @@ async function createStatus(req, res) {
         statusId: mongoose.Types.ObjectId(),
         comment: req.body.comment,
         image: "no image",
-        userId: "sdsdsd",
+        userId: req.user.userId
     })
 
     await status
@@ -28,4 +28,23 @@ async function createStatus(req, res) {
     });
 }
 
-module.exports = {createStatus}
+async function getAllStatus(req, res) {
+  statusModel.find()
+    .select('_id statusId image userId')
+    .then((allStatus) => {
+      return res.status(200).json({
+        success: true,
+        message: 'A list of all status',
+        Status: allStatus,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: 'Server error. Please try again.',
+        error: err.message,
+      });
+    });
+}
+
+module.exports = {createStatus, getAllStatus}
--- StudentPortal/routes/status.js
+++ StudentPortal/routes/status.js
@@ -1,16 +1,11 @@
 const express = require('express')
 const passport = require('passport')
 const router = express.Router()
-const {createStatus} = require('../controllers/statusController')
+const {createStatus, getAllStatus} = require('../controllers/statusController')
 const {authenticateToken} = require('../config/token')
 // api status: GET POST PUT DELETE
 
-router.get('/',(req, res) => {
-    res.json("success")
-})
-
-router.post('/',createStatus, authenticateToken, function(req, res, next) {
-    res.json("success")
-})
+router.get('/', getAllStatus, authenticateToken)
+router.post('/',createStatus, authenticateToken)
 
 module.exports = router
