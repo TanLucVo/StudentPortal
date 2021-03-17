63b6a912ab0f1ed9c0f593e83a6bd9312769bf94
HuynhLoi <caoboiloi@users.noreply.github.com>
2021-03-17 11:36:24 +0700

    completed api status

--- StudentPortal/app.js
+++ StudentPortal/app.js
@@ -24,7 +24,7 @@ app.use(
 	})
 )
 
-
+app.use(express.static('public'))
 
 //passport
 const passport = require('passport')
--- StudentPortal/controllers/statusController.js
+++ StudentPortal/controllers/statusController.js
@@ -29,8 +29,8 @@ async function createStatus(req, res) {
 }
 
 async function getAllStatus(req, res) {
-  statusModel.find()
-    .select('_id statusId image userId')
+  await statusModel.find()
+    .select('_id statusId comment image userId')
     .then((allStatus) => {
       return res.status(200).json({
         success: true,
@@ -48,26 +48,63 @@ async function getAllStatus(req, res) {
 }
 
 async function getStatusId(req, res) {
-  const id = req.params.statusId;
-  statusModel.findById(id)
-    .then((singleStatus) => {
-      res.status(200).json({
-        success: true,
-        message: `More on ${singleStatus.statusId}`,
-        Course: singleStatus,
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        success: false,
-        message: 'This status does not exist in database',
-        error: err.message,
-      });
-   });
+  const id = req.params.id
+  await statusModel.findById(id)
+  .then((singleStatus) => {
+    res.status(200).json({
+      success: true,
+      message: `More on ${singleStatus.statusId}`,
+      Course: singleStatus,
+    });
+  })
+  .catch((err) => {
+    res.status(500).json({
+      success: false,
+      message: 'This status does not exist in database',
+      error: err.message,
+    });
+  });
 }
 
 async function deleteStatusId(req, res) {
-  res.json('success')
+  const id = req.params.id
+  await statusModel.findByIdAndRemove(id)
+    .exec()
+    .then(()=> res.status(204).json({
+      success: true,
+    }))
+    .catch((err) => res.status(500).json({
+      success: false,
+    }));
 }
 
-module.exports = {createStatus, getAllStatus, getStatusId, deleteStatusId}
+async function updateStatusId(req, res) {
+  const log =  await statusModel.findOneAndUpdate(
+    {_id: req.params.id},
+    {
+      comment: req.body.comment
+    },
+    {
+      useFindAndModify: false,
+      upsert: false,
+      new: true
+    }
+  )
+  .exec()
+  .then((oldStatus) => {
+    return res.status(200).json({
+      success: true,
+      message: 'this status was updated successfully',
+      Status: oldStatus,
+    });
+  })
+  .catch((err) => {
+    res.status(500).json({
+      success: false,
+      message: 'Server error. Please try again.',
+      error: err.message,
+    });
+  });
+}
+
+module.exports = {createStatus, getAllStatus, getStatusId, deleteStatusId, updateStatusId}
