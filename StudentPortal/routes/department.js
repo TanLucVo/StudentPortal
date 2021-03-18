f90498a8df408be7e10f1cd15eb2475c689e7e4f
Tấn Lực Võ <57494001+TanLucVo@users.noreply.github.com>
2021-03-18 13:21:39 +0700

    moi co dien moi up lai ne

--- StudentPortal/routes/department.js
+++ StudentPortal/routes/department.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+const fs = require('fs'); 
 /* GET login page. */
 router.get('/', async function (req, res, next) {
-	res.render('dashboardDepartment',{user: req.user});
+	const data = fs.readFileSync(path.join(__dirname, '..','/views/partial/showDepartment.ejs'))
+	res.render('dashboardDepartment',{user: req.user, content:data});
 });
 
 //post
 router.get('/add', async function (req, res, next) {
-	res.render('dashboardDepartment',{user: req.user});
+	if(req.user.type !=="admin") return res.send('Access dineid')
+	res.render('addDepartment',{user: req.user});
 });
 
 router.post('/add', async function (req, res, next) {
-	res.render('dashboardDepartment',{user: req.user});
+	if(req.user.type !=="admin") return res.status(403).json('Access dineid')
+	res.render('addDepartment',{user: req.user});
 });
 
 
