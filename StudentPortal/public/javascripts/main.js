79ebc825f9314fb9d4af0388ce142911edcf2cd9
Tấn Lực Võ <57494001+TanLucVo@users.noreply.github.com>
2021-04-01 16:03:36 +0700

    Update main.js

--- StudentPortal/public/javascripts/main.js
+++ StudentPortal/public/javascripts/main.js
@@ -419,11 +419,11 @@ const getPassedTime = (startTime, endTime) => {
         outputTime = passTime + " giây trước"
     else if (passTime < (60 * 60))
         outputTime = Math.floor(passTime / 60) + " phút trước"
-    else if (passTime < (60 * 60 * 60))
+    else if (passTime < (60 * 60 * 24))
         outputTime = Math.floor(passTime / (60 * 60)) + " giờ trước"
-    else if (passTime < (60 * 60 * 60 * 24))
+    else if (passTime < (60 * 60 * 24 * 30))
         outputTime = Math.floor(passTime / (60 * 60 * 24)) + " ngày trước"
-    else if (passTime < (60 * 60 * 60 * 24 * 30))
+    else if (passTime < (60 * 60 * 24 * 30 *365))
         outputTime = Math.floor(passTime / (60 * 60 * 24 * 30)) + " tháng trước"
     return outputTime
 }
