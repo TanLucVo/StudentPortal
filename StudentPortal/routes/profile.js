c6708723548a328b788e16dd46be725dd0084192
Huynh Loi <caoboiloi@gmail.com>
2021-05-15 17:15:48 +0700

    d

--- StudentPortal/routes/profile.js
+++ StudentPortal/routes/profile.js
@@ -6,18 +6,19 @@ const {authenticateToken} = require('../config/token')
 const moment = require('moment')
 
 const userModel = require('../models/user')
+const statusModel = require('../models/status')
 
 router.get('/',authenticateToken,async (req, res, next) => {
     var {id} = req.query
     let mainUser = req.user
-    let checkUser = false
+    let statusUser = await statusModel.find({user: id})
     if (id != req.user._id) {
         mainUser = await userModel.findById(id)
     }
-    console.log(checkUser)
     res.render('profile', {
         user: req.user,
         mainUser,
+        statusUser,
         moment
     })
 })
